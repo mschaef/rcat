5553b66a33aefdd75ed3e73b3ffd4ca0e0636a48
Mike Schaeffer <mike@mschaef.com>
2021-11-11 11:26:12 -0500

    Clean up status messaging.

--- rcat.c
+++ rcat.c
@@ -30,17 +30,21 @@ int main(int argc, char *argv[]) {
      read_ofs = 0;
 
      while(1) {
+          if (read_ofs % (1024 * 1024) == 0) {
+               fprintf(stderr, "ofs=%zu\n", read_ofs);
+          }
+
           lseek(in, read_ofs, SEEK_SET);
           size_t bytes_read;
 
           bytes_read = read(in, block_buf, BLOCK_SIZE);
 
-          fprintf(stderr, "ofs=%zu, n=%zu, errno=%s\n",
-                  read_ofs, bytes_read, strerror(errno));
-
           if (bytes_read == 0) {
                break;
           } else if (bytes_read == -1) {
+               fprintf(stderr, "ofs=%zu, n=%zu, errno=%s\n",
+                       read_ofs, bytes_read, strerror(errno));
+
                if (EINTR == errno) {
                     continue;
                } else if (EIO == errno) {
