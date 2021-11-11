775ff087c4acb6eb04653817bbca0ddf388fa2ad
Mike Schaeffer <mike@mschaef.com>
2021-11-11 11:22:38 -0500

    Add error detection and block filling.

--- rcat.c
+++ rcat.c
@@ -23,19 +23,35 @@ int main(int argc, char *argv[]) {
      }
 
      char block_buf[BLOCK_SIZE];
+
+     size_t read_ofs;
      size_t n;
 
-     while(1) {
-          size_t bytes_read = read(in, block_buf, BLOCK_SIZE);
+     read_ofs = 0;
 
-          fprintf(stderr, "n=%zu, errno=%d\n", bytes_read, errno);
+     while(1) {
+          lseek(in, read_ofs, SEEK_SET);
+          size_t bytes_read;
+
+          bytes_read = read(in, block_buf, BLOCK_SIZE);
+
+          fprintf(stderr, "ofs=%zu, n=%zu, errno=%s\n",
+                  read_ofs, bytes_read, strerror(errno));
 
           if (bytes_read == 0) {
                break;
+          } else if (bytes_read == -1) {
+               if (EINTR == errno) {
+                    continue;
+               } else if (EIO == errno) {
+                    memset(block_buf, 0, BLOCK_SIZE);
+                    bytes_read = BLOCK_SIZE;
+               }
+          } else {
+               fwrite(block_buf, 1, bytes_read, stdout);
           }
 
-
-          fwrite(block_buf, 1, bytes_read, stdout);
+          read_ofs += bytes_read;
      }
 
      close(in);
